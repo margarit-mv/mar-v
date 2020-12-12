b156701155a0ad39b9feb8b8857cc99846ca787b
Student <Student@lab4-10.kkitiu.local>
2020-12-12 10:20:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self,color):
+        self.color = color
+
+class LightweighCar(Car):
+    def
+        __init__(self,color, door_amount):
+        super().__init__(color)
+        self.door_amount = door_amount:
